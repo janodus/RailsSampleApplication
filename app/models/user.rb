895aabd1c05bca383051b89167c3451afaf8bd4b
jan <janodusv@gmail.com>
2016-09-07 20:19:16 -0700

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -6,7 +8,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string.
  def User.digest(string)
@@ -14,5 +16,28 @@ class User < ApplicationRecord
                                                  BCrypt::Engine.cost
    BCrypt::Password.create(string, cost: cost)
  end
+
+ # Creates a new 'remember me' token
+ def User.new_token
+   SecureRandom.urlsafe_base64
+ end
+
+ # Remembers a user in the database for use in persistent sessions.
+ def remember
+   self.remember_token = User.new_token
+   update_attribute(:remember_digest, User.digest(remember_token))
+ end
+
+ # Returns TRUE if the given token matches the digest (user's own pw digest)
+ def authenticated?(remember_token)
+   return false if remember_digest.nil?
+   BCrypt::Password.new(remember_digest).is_password?(remember_token)
+ end
+
+ # Forgets a user.
+ def forget
+   update_attribute(:remember_digest, nil)
+ end
+
 
 end
